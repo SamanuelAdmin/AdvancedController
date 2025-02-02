264cd32d90e0b92f4a0df63a4b3d33413608a063
Samanuel Admin <68198268+SamanuelAdmin@users.noreply.github.com>
2025-02-02 17:47:03 +0200

    fix logger (errors)

--- src/main/java/org/controller/codetemplates/LoggerInterface.java
+++ src/main/java/org/controller/codetemplates/LoggerInterface.java
@@ -2,5 +2,5 @@ package org.controller.codetemplates;
 
 public interface LoggerInterface {
     public default void log(String... args) {};
-    public default void error(String message, Object... args) {};
+    public default void error(String... message) {};
 }
